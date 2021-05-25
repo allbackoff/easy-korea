0bbdfc78732e94ee6c2cfb1389341ba07c85c830
Alisher <alishsem@gmail.com>
2021-05-26 03:19:18 +0900

    added three question templates

--- src/components/Forum.js
+++ src/components/Forum.js
@@ -79,6 +79,53 @@ function Forum() {
 
                     <div className="col-md-6" style={{ marginTop: "20px", marginBottom: "10px"  }}    >
                         <p>Questions</p>
+                        
+                            <div className="question">
+                                <div className="title">
+                                    <h6>Dentistry in Ulsan</h6>
+                                </div>
+                                <div className="content">
+                                    <p>Looking for the cheapest option to save my poor teeth in Ulsan</p>
+                                </div>
+                                <div className="date">
+                                    <p>Posted on: March 26, 2021, Jeph Bessos</p>
+                                </div>
+                                
+                                    
+                                
+                                
+                            </div>
+                       
+                        <div className="question">
+                            <div className="title">
+                                <h6>Dentistry in Ulsan</h6>
+                            </div>
+                            <div className="content">
+                                <p>Looking for the cheapest option to save my poor teeth in Ulsan</p>
+                            </div>
+                            <div className="date">
+                                <p>Posted on: March 26, 2021, By Adam Saydlen</p>
+                            </div>
+
+
+
+
+                        </div>
+                        <div className="question">
+                            <div className="title">
+                                <h6>Dentistry in Ulsan</h6>
+                            </div>
+                            <div className="content">
+                                <p>Looking for the cheapest option to save my poor teeth in Ulsan</p>
+                            </div>
+                            <div className="date">
+                                <p>Posted on: March 26, 2021, By Kyle Mbappe </p>
+                            </div>
+
+
+
+
+                        </div>
                     </div>
 
                     <div className="col-md-3" style={{ marginTop: "20px", marginBottom: "10px"  }}    >
