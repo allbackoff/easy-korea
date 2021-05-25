b7732d8ab2c822efe2d46ecb3abfc8beb5c7773e
Alisher <alishsem@gmail.com>
2021-05-26 02:27:57 +0900

    added media images to the emergency received section

--- src/components/EmergencyReceiveConfirmation.js
+++ src/components/EmergencyReceiveConfirmation.js
@@ -9,16 +9,44 @@ function EmergencyReceiveConfirmation() {
     }
     return (
         <div className="container">
+            <Breadcrumb className="top">
+                <BreadcrumbItem>
+                    <Link to="/emergency" className="link">Emergency</Link>
+                </BreadcrumbItem>
+                <BreadcrumbItem active>
+                    Receive Help
+                </BreadcrumbItem>
+            </Breadcrumb>
             <div className="main">
             <h1>Here are some people that can help you right now!</h1>
-                <Media tag="li">
+                <Media >
                     <Media left middle>
-                        <Media object src={'/images/photo.jpg'} alt={'name'} style={imgStyle} />
+                        <Media object src={'/images/photo.png'} alt={'name'} style={imgStyle} />
                     </Media>
                     <Media body className="ml-5">
                         <Media heading>Leonel Mesi</Media>
                         <h6>3 years in Korea, professional footballer</h6>
                         <p>Phone Number: +7 777 777 77</p>
+                    </Media>
+                </Media>
+                <Media >
+                    <Media left middle>
+                        <Media object src={'/images/photo.png'} alt={'name'} style={imgStyle} />
+                    </Media>
+                    <Media body className="ml-5">
+                        <Media heading>Jasan Steitem</Media>
+                        <h6>2 years in Korea, actor</h6>
+                        <p>Phone Number: +7 454 233 34</p>
+                    </Media>
+                </Media>
+                <Media >
+                    <Media left middle>
+                        <Media object src={'/images/photo.png'} alt={'name'} style={imgStyle} />
+                    </Media>
+                    <Media body className="ml-5">
+                        <Media heading>Ilon Mask</Media>
+                        <h6>6 years in Korea, entrepreneur</h6>
+                        <p>Phone Number: +7 876 543 23</p>
                     </Media>
                 </Media>
             </div>
