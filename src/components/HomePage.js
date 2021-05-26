5ac51b08e4db673dc95bdbb9cc0ae615161c076b
allbackoff <albakov.k@gmail.com>
2021-05-26 12:10:28 +0900

    fixed spelling mistake

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -7,7 +7,7 @@ function HomePage(styles) {
         <div className = "homepage">
             <p className = "greeting">Welcome to Easy Korea!</p>
             <div className = "subinfo">
-            <p className = "info"> All the informtation needed for living in Korea comfortly!</p>
+            <p className = "info"> All the information needed for living in Korea comfortly!</p>
             </div>
         </div>
         </div>
