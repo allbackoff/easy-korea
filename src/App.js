8f7bed3f7e404cff7e59c4b7fe8fb10aabc24486
allbackoff <albakov.k@gmail.com>
2021-05-26 18:49:08 +0900

    show images that were not shown before

--- src/App.js
+++ src/App.js
@@ -1,163 +1,179 @@
-import './App.css';
-import fire from './fire'
-import styles from  './styles.css'
-import Header from './components/Header'
-import HomePage from './components/HomePage'
-import Page from './components/Page'
-import React, {useState, useEffect} from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
-import Login from './components/Login'
-import Guides from './components/Guides';
-import Forum from './components/Forum';
-import Forum1 from './components/Forum1';
-import HospitalGuide from './components/HospitalGuide'
-import AirportGuide from './components/AirportGuide'
-import DocumentsGuide from './components/DocumentsGuide'
-import FoodGuide from './components/FoodGuide'
-import TransportGuide from './components/TransportGuide'
+import "./App.css";
+import fire from "./fire";
+import styles from "./styles.css";
+import Header from "./components/Header";
+import HomePage from "./components/HomePage";
+import Page from "./components/Page";
+import React, { useState, useEffect } from "react";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Login from "./components/Login";
+import Guides from "./components/Guides";
+import Forum from "./components/Forum";
+import Forum1 from "./components/Forum1";
+import HospitalGuide from "./components/HospitalGuide";
+import AirportGuide from "./components/AirportGuide";
+import DocumentsGuide from "./components/DocumentsGuide";
+import FoodGuide from "./components/FoodGuide";
+import TransportGuide from "./components/TransportGuide";
 
-import visa from'./components/Documents/visa'
-import arc from './components/Documents/arc'
-import residency from './components/Documents/residency'
+import visa from "./components/Documents/visa";
+import arc from "./components/Documents/arc";
+import residency from "./components/Documents/residency";
 
-import Emergency from './components/Emergency'
-import EmergencyReceive from './components/EmergencyReceive'
-import EmergencyGive from './components/EmergencyGive'
-import EmergencyConfirm from './components/EmergencyConfirm'
-import EmergencyHospital from './components/EmergencyHospital'
-import EmergencyReceiveConfirmation from './components/EmergencyReceiveConfirmation'
-import AskQuestion from './components/AskQuestion'
-
+import Emergency from "./components/Emergency";
+import EmergencyReceive from "./components/EmergencyReceive";
+import EmergencyGive from "./components/EmergencyGive";
+import EmergencyConfirm from "./components/EmergencyConfirm";
+import EmergencyHospital from "./components/EmergencyHospital";
+import EmergencyReceiveConfirmation from "./components/EmergencyReceiveConfirmation";
+import AskQuestion from "./components/AskQuestion";
 
 function App() {
-  const [user, setUser] = useState('')
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [emailError, setEmailError] = useState('')
-  const [passwordError, setPasswordError] = useState('')
-  const [hasAccount, setHasAccount] = useState(false)
-  
+  const [user, setUser] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [emailError, setEmailError] = useState("");
+  const [passwordError, setPasswordError] = useState("");
+  const [hasAccount, setHasAccount] = useState(false);
 
   const clearInputs = () => {
-    setEmail('')
-    setPassword('')
-  }
+    setEmail("");
+    setPassword("");
+  };
 
   const clearErrors = () => {
-    setEmailError('')
-    setPasswordError('')
-  }
+    setEmailError("");
+    setPasswordError("");
+  };
 
   const handleLogin = () => {
-    clearErrors()
+    clearErrors();
     fire
       .auth()
       .signInWithEmailAndPassword(email, password)
       .catch((error) => {
-        switch(error.code){
-          case 'auth/invalid-email':
-          case 'auth/user-disabled':
-          case 'auth/user-not-fount':
-            setEmailError(error.message)
-            break
-          case 'auth/wrong-password':
-            setPasswordError(error.message)
-            break
+        switch (error.code) {
+          case "auth/invalid-email":
+          case "auth/user-disabled":
+          case "auth/user-not-fount":
+            setEmailError(error.message);
+            break;
+          case "auth/wrong-password":
+            setPasswordError(error.message);
+            break;
         }
-      })
-  }
+      });
+  };
 
   const handleSignup = () => {
-    clearErrors()
+    clearErrors();
     fire
       .auth()
       .createUserWithEmailAndPassword(email, password)
       .catch((error) => {
-        switch(error.code){
-          case 'auth/email-already-in-use':
-          case 'auth/invalid-email':
-            setEmailError(error.message)
-            break
-          case 'auth/weak-password':
-            setPasswordError(error.message)
-            break
+        switch (error.code) {
+          case "auth/email-already-in-use":
+          case "auth/invalid-email":
+            setEmailError(error.message);
+            break;
+          case "auth/weak-password":
+            setPasswordError(error.message);
+            break;
         }
-      })
-  }
+      });
+  };
 
   const handleLogout = () => {
-    fire.auth().signOut()
-  }
+    fire.auth().signOut();
+  };
 
   const authListener = () => {
     fire.auth().onAuthStateChanged((user) => {
-      if(user) {
-        clearInputs()
-        setUser(user)
+      if (user) {
+        clearInputs();
+        setUser(user);
       } else {
-        setUser('')
+        setUser("");
       }
-    })
-  }
+    });
+  };
 
   useEffect(() => {
-    authListener()
-  }, [])
+    authListener();
+  }, []);
 
   return (
     <Router>
       <div className="App">
-        <Header styles = {styles} user={user}/>
+        <Header styles={styles} user={user} />
         <Switch>
-            {!user ? (
-              <Route path="/login" render={(props) => (
-                <Login {...props}
-                 email={email}
-                 setEmail={setEmail}
-                 password={password}
-                 setPassword={setPassword}
-                 handleLogin={handleLogin}
-                 handleSignup={handleSignup}
-                 hasAccount={hasAccount}
-                 setHasAccount={setHasAccount}
-                 emailError={emailError}
-                 passwordError={passwordError}
-                 user={user}
-                 handleLogout={handleLogout}
-                 />
-              )} />
-            ) : (
-              <Route path="/mypage" render={(props) => (
-                <Page {...props} 
-                handleLogout={handleLogout}
-                user={user}
+          {!user ? (
+            <Route
+              path="/login"
+              render={(props) => (
+                <Login
+                  {...props}
+                  email={email}
+                  setEmail={setEmail}
+                  password={password}
+                  setPassword={setPassword}
+                  handleLogin={handleLogin}
+                  handleSignup={handleSignup}
+                  hasAccount={hasAccount}
+                  setHasAccount={setHasAccount}
+                  emailError={emailError}
+                  passwordError={passwordError}
+                  user={user}
+                  handleLogout={handleLogout}
                 />
-              )} />
-            )}
-            <Route exact path="/guides" component={Guides}/>
-            <Route exact path="/guides/hospital" component={HospitalGuide} />
-            <Route exact path="/guides/airport" component={AirportGuide} />
-            <Route exact path="/guides/documents" component={DocumentsGuide} />
-            <Route exact path="/forum" component={Forum} />
-            <Route exact path="/forum/ask" component={AskQuestion} />
-            <Route exact path="/forum1" component={Forum1} />
-            <Route exact path="/guides/food" component={FoodGuide}/>
-            <Route exact path="/guides/transport" component={TransportGuide} />
+              )}
+            />
+          ) : (
+            <Route
+              path="/mypage"
+              render={(props) => (
+                <Page {...props} handleLogout={handleLogout} user={user} />
+              )}
+            />
+          )}
+          <Route exact path="/guides" component={Guides} />
+          <Route exact path="/guides/hospital" component={HospitalGuide} />
+          <Route exact path="/guides/airport" component={AirportGuide} />
+          <Route exact path="/guides/documents" component={DocumentsGuide} />
+          <Route exact path="/forum" component={Forum} />
+          <Route exact path="/forum/ask" component={AskQuestion} />
+          <Route exact path="/forum1" component={Forum1} />
+          <Route exact path="/guides/food" component={FoodGuide} />
+          <Route exact path="/guides/transport" component={TransportGuide} />
 
-            <Route exact path="/guides/documents/arc" component={arc} />
-            <Route exact path="/guides/documents/visa" component={visa} />
-            <Route exact path="/guides/documents/residency" component={residency} />
+          <Route exact path="/guides/documents/arc" component={arc} />
+          <Route exact path="/guides/documents/visa" component={visa} />
+          <Route
+            exact
+            path="/guides/documents/residency"
+            component={residency}
+          />
 
-            <Route exact path="/emergency" component={Emergency} />
-            <Route exact path="/emergency/receive" component={EmergencyReceive} />
-            <Route exact path="/emergency/give" component={EmergencyGive} />
-            <Route exact path="/emergency/give/confirm" component={EmergencyConfirm} />
-            <Route exact path="/emergency/hospital" component={EmergencyHospital} />
-            <Route path = "/emergency/receive/" component = {EmergencyReceiveConfirmation}/>
+          <Route exact path="/emergency" component={Emergency} />
+          <Route exact path="/emergency/receive" component={EmergencyReceive} />
+          <Route exact path="/emergency/give" component={EmergencyGive} />
+          <Route
+            exact
+            path="/emergency/give/confirm"
+            component={EmergencyConfirm}
+          />
+          <Route
+            exact
+            path="/emergency/hospital"
+            component={EmergencyHospital}
+          />
+          <Route
+            path="/emergency/receive/"
+            component={EmergencyReceiveConfirmation}
+          />
 
-            <Route path="/" component={HomePage}/>
+          <Route path="/" component={HomePage} />
         </Switch>
-        
       </div>
     </Router>
   );
